b6965c22fd516f2db8ff1718926c9aa01665f313
Marco Souza <marcoasouza.1@gmail.com>
2015-02-05 14:56:40 -0300

    initial commit

---
+++ app/src/main/java/br/com/jera/hackathonford/activities/BaseActivity.java
@@ -0,0 +1,17 @@
+package br.com.jera.hackathonford.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by marco on 05/02/15.
+ */
+public class BaseActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        //TODO methods for all activities should be here
+    }
+}
