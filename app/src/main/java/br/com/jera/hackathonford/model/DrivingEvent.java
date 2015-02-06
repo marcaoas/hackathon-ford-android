6511bcd596aca0e0da1714f3793f760a182a5454
Marco Souza <marcoasouza.1@gmail.com>
2015-02-06 10:21:14 -0300

    status de atualizacao

--- app/src/main/java/br/com/jera/hackathonford/model/DrivingEvent.java
+++ app/src/main/java/br/com/jera/hackathonford/model/DrivingEvent.java
@@ -3,6 +3,7 @@ package br.com.jera.hackathonford.model;
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
 
 import java.util.Date;
 
@@ -11,6 +12,8 @@ import java.util.Date;
  */
 @Table(name="DrivingEvents")
 public class DrivingEvent extends Model {
+
+    public static final Double SPEED_LIMIT = 100d;
 
     @Column(name = "eventType")
     public String eventType; //DrivingEventType enum
@@ -21,9 +24,23 @@ public class DrivingEvent extends Model {
     @Column(name = "lng")
     public Double lng;
 
+    @Column(name = "speed")
+    public Double speed;
+
     @Column(name = "datetime")
     public Date datetime;
 
     @Column(name = "airbagStatus")
     public String airbagStatus;
+
+    public static DrivingEvent getLast(){
+        return new Select().from(DrivingEvent.class).orderBy("id desc").executeSingle();
+    }
+
+    public boolean isOverSpeed() {
+        if(speed!=null){
+            return speed > (1.1 * SPEED_LIMIT);
+        }
+        return false;
+    }
 }
